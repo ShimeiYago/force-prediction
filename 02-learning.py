97b63869bc73e216fbdc414c089c04a96ae71982
Shimei Yago <shimei@Shimeis-MacBook-Pro.local>
2020-05-17 14:02:13 +0900

    add function model-saving

--- 02-learning.py
+++ 02-learning.py
@@ -4,29 +4,35 @@ import numpy as np
 import os
 import argparse
 from sklearn.model_selection import train_test_split
-
 from tensorflow.keras.callbacks import LearningRateScheduler
+from tensorflow.keras.callbacks import ModelCheckpoint
 import models.DNN as DNN
+
+OUTDIR_WEIGHT = "workspace/02-learning/weight"
+OUTDIR_HISTORY = "workspace/02-learning/history"
+LEARNING_RATE = 0.001
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', default="workspace/01-make-dataset/allatom/dataset.npz", help='input trajectory (.npz)')
-    parser.add_argument('-o', '--out', default="workspace/02-learning/history.npy", help='output file path(.npy)')
+    parser.add_argument('-d', '--data', default="workspace/01-make-dataset/allatom/dataset.npz", help='input trajectory (.npz)')
     parser.add_argument('-e', '--epochs', type=int, default=100, help='epochs')
     parser.add_argument('-b', '--batch', type=int, default=50, help='batch size')
+    parser.add_argument('-i', '--learning_index', type=int, default=0, help='learning index')
     args = parser.parse_args()
 
-    os.makedirs(os.path.dirname(args.out), exist_ok=True)
-
-
-    ### prepare data ###
+    # make dir
+    history_path = os.path.join(OUTDIR_WEIGHT, f'history{args.learning_index:0=3}')
+    os.makedirs(OUTDIR_WEIGHT, exist_ok=True)
+    os.makedirs(OUTDIR_HISTORY, exist_ok=True)
 
     # expranatory
-    x = np.load(args.input)['x']
+    npz = np.load(args.data)
+    x = npz['x']
 
     # response
-    t = np.load(args.input)['y']
+    t = np.load(args.data)['y']
+    del npz
 
     # data split
     x_train, x_val, t_train, t_val = train_test_split(x, t,
@@ -34,33 +40,43 @@ def main():
                                                       shuffle=True)
 
     # learning
-    model = DNN.model2()
-    lr_callback = LearningRateScheduler(lr_decay(args.epochs))
+    model = DNN.model2(input_dim=x.shape[1], learning_rate=LEARNING_RATE)
+
+    # take over previous weights
+    if args.learning_index > 0:
+        preindex = args.learning_index - 1
+        model.load_weights(os.path.join(OUTDIR_WEIGHT, f'weight{preindex:0=3}.hdf5'))
+    
 
     hist = model.fit(x_train, t_train,
                      epochs=args.epochs,
                      batch_size=args.batch,
                      verbose=2,
-                     validation_data=(x_val, t_val),
-                     callbacks=[lr_callback])
+                     validation_data=(x_val, t_val))
 
-    np.save(args.out, hist.history)
+    # save weights
+    weight_path = os.path.join(OUTDIR_WEIGHT, f'weight{args.learning_index:0=3}.hdf5')
+    model.save_weights(weight_path)
+
+    # save history
+    history_path = os.path.join(OUTDIR_HISTORY, f'history{args.learning_index:0=3}')
+    np.save(history_path, hist.history)
 
 
-class lr_decay:
-    def __init__(self, n_epochs):
-        self.n_epochs = n_epochs
+def get_learning_index():
+    objective_dirpath = os.path.join(OUTDIR, 'weights')
+    indexlist = [int(f) for f in os.listdir(objective_dirpath) 
+                 if os.path.isdir(os.path.join(objective_dirpath, f))]
+    
+    learning_index = 0
+    for i in range(1000):
+        if i in indexlist:
+            continue
 
-    def __call__(self, epoch):
-        x = 0.01
+        learning_index = i
+        break
 
-        if epoch >= (self.n_epochs * 0.5):
-            x = 0.001
-
-        if epoch >= (self.n_epochs * 0.75):
-            x = 0.0001
-
-        return x
+    return learning_index
 
 
 if __name__ == '__main__':
--- models/DNN.py
+++ models/DNN.py
@@ -1,11 +1,12 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import InputLayer, Dense, Dropout
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras import optimizers
 
 
-def model1():
+def model1(input_shape, learning_rate=0.001):
     model = Sequential()
+    model.add(InputLayer(input_shape=input_shape))
     model.add(Dense(100, activation='relu'))
     model.add(Dense(50, activation='relu'))
     model.add(Dense(30, activation='relu'))
@@ -13,22 +14,23 @@ def model1():
     model.add(Dropout(0.5))
     model.add(Dense(3, activation='linear'))      
 
-    optimizer = optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=True)
+    optimizer = optimizers.Adam(learning_rate=learning_rate, beta_1=0.9, beta_2=0.999, amsgrad=True)
     model.compile(optimizer=optimizer, loss='mean_squared_error')
 
     return model
 
 
-def model2():
+def model2(input_dim, learning_rate=0.001):
     model = Sequential()
-    model.add(Dense(200, activation='relu'))
+    # model.add(InputLayer(input_shape=input_shape))
+    model.add(Dense(200, input_dim=input_dim, activation='relu'))
     model.add(Dense(100, activation='relu'))
     model.add(Dense(50, activation='relu'))
     model.add(Dense(30, activation='relu'))
     model.add(Dense(10, activation='relu'))
     model.add(Dense(3, activation='linear'))      
 
-    optimizer = optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=True)
+    optimizer = optimizers.Adam(learning_rate=learning_rate, beta_1=0.9, beta_2=0.999, amsgrad=True)
     model.compile(optimizer=optimizer, loss='mean_squared_error')
 
     return model
