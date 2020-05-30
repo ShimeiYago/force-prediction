a9e4b17807d0b35516f861f84aa3c6e2a8796cca
Shimei Yago <shimei@Shimeis-MacBook-Pro.local>
2020-05-30 20:50:43 +0900

    add models

--- models/DNN.py
+++ models/DNN.py
@@ -5,11 +5,13 @@ from tensorflow.keras import optimizers
 
 def model1(input_dim, learning_rate=0.001):
     model = Sequential()
-    model.add(Dense(100, input_dim=input_dim, activation='relu'))
+    model.add(Dense(200, input_dim=input_dim, activation='relu'))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(100, activation='relu'))
     model.add(Dense(50, activation='relu'))
     model.add(Dense(30, activation='relu'))
     model.add(Dense(10, activation='relu'))
-    model.add(Dropout(0.5))
     model.add(Dense(3, activation='linear'))      
 
     optimizer = optimizers.Adam(learning_rate=learning_rate, beta_1=0.9, beta_2=0.999, amsgrad=True)
@@ -32,3 +34,19 @@ def model2(input_dim, learning_rate=0.001):
 
     return model
 
+
+def model3(input_dim, learning_rate=0.001):
+    model = Sequential()
+    model.add(Dense(300, input_dim=input_dim, activation='relu'))
+    model.add(Dense(200, activation='relu'))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(30, activation='relu'))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dense(3, activation='linear'))      
+
+    optimizer = optimizers.Adam(learning_rate=learning_rate, beta_1=0.9, beta_2=0.999, amsgrad=True)
+    model.compile(optimizer=optimizer, loss='mean_squared_error')
+
+    return model
