c34765674edfe0489dd9eaf9c3d7c0a42d9de544
Shimei Yago <shimei@Shimeis-MacBook-Pro.local>
2020-11-06 16:24:38 +0900

    update visalize history

--- visualize-history.py
+++ visualize-history.py
@@ -4,10 +4,13 @@ import numpy as np
 import os
 import glob
 import matplotlib.pyplot as plt
+import h5py
+
 
 INPUTDIR02 = "workspace/02-lrtest"
 INPUTDIR03 = "workspace/03-learning"
 
+plt.rcParams["font.size"] = 18
 
 def main():
     # load 02 data
@@ -15,7 +18,8 @@ def main():
     historydict03 = load_history_csv(glob.glob(INPUTDIR03 + '/*/*/history.csv'))
 
     # plot 02
-    for outpath, history in historydict02.items():
+    for outpath, li in historydict02.items():
+        _, history = li
         history = np.array(history)[:, [1, 2]].transpose(1, 0)
         loss = history[0]
         lrs = [str(lr) for lr in history[1]]
@@ -30,7 +34,14 @@ def main():
         plt.savefig(outpath)
 
     # plot 03
-    for outpath, history in historydict03.items():
+    for outpath, li in historydict03.items():
+        atom, history = li
+
+        # root and denormalize
+        mean, std = get_mean_std(outpath, atom)
+        history = np.sqrt(history)
+        history = np.add(np.multiply(history, std), mean)
+
         history = np.array(history)[:, [1, 3]].transpose(1, 0)
         loss = history[0]
         val_loss = history[1]
@@ -41,7 +52,6 @@ def main():
         ax = fig.add_subplot(111)
         ax.plot(epochs, loss, label='train')
         ax.plot(epochs, val_loss, label='validation')
-        ax.plot(epochs, [1]*len(epochs), color='k', linewidth=0.5)
         plt.xlabel('epoch')
         plt.ylabel('loss')
         plt.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=0)
@@ -55,12 +65,26 @@ def load_history_csv(filepath_list):
         filename = os.path.splitext(os.path.basename(filepath))[0] + '.png'
         outpath = os.path.join(os.path.dirname(filepath), filename)
 
+        atom = os.path.basename((os.path.dirname(os.path.dirname(filepath))))
+
         try:
-            historydict[outpath] = np.loadtxt(filepath, delimiter=',', skiprows=1)
+            historydict[outpath] = [atom, np.loadtxt(filepath, delimiter=',', skiprows=1)]
         except:
             print(f'"{filepath}" are skipped. cannot load the file.')
-        
+
     return historydict
+
+
+def get_mean_std(filepath, atom):
+    option_path = os.path.join(os.path.dirname(filepath), 'option.txt')
+    with open(option_path) as f:
+        datasetpath = f.readline().split('\t')[1].strip()
+
+    ## normalization values ## #
+    with h5py.File(datasetpath, mode='r') as f:
+        y_mean, y_std = f[f'/{atom}/normalization'][...]
+
+    return y_mean, y_std
 
 if __name__ == '__main__':
     main()
